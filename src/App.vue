81bf7915c093c6ddbd0ceca83ef8464ee628e924
Miguel Henrique <56663660+devmhs@users.noreply.github.com>
2021-02-21 00:04:38 -0300

    XXXXXXXXX

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld
-      msg="Welcomdsadsadasdsaddsaaaaaaaaaaaaaaaaaaaaaaaaaae to Your Vue.js App"
+      msg="dsaadsasdaxzzcxxczxczcxxczxczcxzxczxczzxccxzcxzxcxczzxcxzcxczjs App"
     />
   </div>
 </template>
