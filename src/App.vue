9d5974a056df4514c15fe787995fde68405b070f
Miguel Henrique <sinqia.miguel@hypeflame.com.br>
2021-02-21 00:03:35 -0300

    dasdsa

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld
-      msg="Welcomdsadsadasdsaddsaaaaaaaaaaaaaaaaaaaaaaaaaae to Your Vue.js App"
-    />
+    <HelloWorld msg="dsadsadsadsadasdsadsa to Your Vue.js App" />
   </div>
 </template>
 
